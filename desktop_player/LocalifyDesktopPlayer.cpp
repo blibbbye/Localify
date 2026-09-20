da372cf42d88b2d98849a7aa9cdbe317e27cf469
blib <100073685+blibbbye@users.noreply.github.com>
2026-09-20 17:05:53 +0200

    Trigger clean desktop player build

--- desktop_player/LocalifyDesktopPlayer.cpp
+++ desktop_player/LocalifyDesktopPlayer.cpp
@@ -196,4 +196,4 @@ int WINAPI wWinMain(HINSTANCE hi,HINSTANCE,LPWSTR,int show){
  ShowWindow(H,show);UpdateWindow(H);SetTimer(H,1,500,0);
  std::thread(server).detach();std::thread(rpcLoop).detach();MSG msg;while(GetMessageW(&msg,0,0,0)>0){TranslateMessage(&msg);DispatchMessageW(&msg);}Run=false;disconnectDiscord();WSACleanup();GdiplusShutdown(GP);return 0;
 }
-// Clean rebuild trigger: stable, resizable Windows desktop player.
+// Clean rebuild trigger: stable, resizable Windows desktop player 4.1.
