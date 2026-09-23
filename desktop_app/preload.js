b271f62565425097a588c82ec02fcfa4bdcc1868
blib <100073685+blibbbye@users.noreply.github.com>
2026-09-23 16:05:58 +0200

    Create HTML Localify Desktop app and optimize large libraries

--- desktop_app/preload.js
+++
@@ -1,8 +0,0 @@
-const {contextBridge,ipcRenderer} = require("electron");
-
-contextBridge.exposeInMainWorld("LocalifyDesktop",{
-  version:"1.0.0",
-  getSaveRoot:()=>ipcRenderer.invoke("localify:save-root"),
-  openFiles:()=>ipcRenderer.invoke("localify:open-files"),
-  launchDiscordPlayer:()=>ipcRenderer.invoke("localify:launch-discord")
-});
